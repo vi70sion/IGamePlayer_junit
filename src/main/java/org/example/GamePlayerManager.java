9e07e4f5548147c71e004fc0c084f0f0d6ce5333
Evaldas L <levaldas@hotmail.com>
2024-05-30 16:29:53 +0300

    Papildyta GamePlayerManagerTest

--- src/main/java/org/example/GamePlayerManager.java
+++ src/main/java/org/example/GamePlayerManager.java
@@ -1,7 +1,42 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GamePlayerManager implements IGamePlayerManager{
 
+    Map<String, String> playersList = new HashMap<>();
+    Map<String, Integer> scoreList = new HashMap<>();
+
+    @Override
+    public void registerPlayer(String playerId, String playerName){
+        if(playerId != null && !playersList.containsKey(playerId)) {
+            playersList.put(playerId, playerName);
+        }
+    }
+
+    @Override
+    public String getPlayerDetails(String playerId) {
+        if(playerId == null || !playersList.containsKey(playerId)) return null;
+        return playersList.get(playerId);
+    }
+
+    @Override
+    public void updatePlayerScore(String playerId, int scoreToAdd) {
+
+    }
+
+    @Override
+    public boolean checkLevelUp(String playerId) {
+        return false;
+    }
+
+    @Override
+    public boolean deletePlayer(String playerId) {
+        if(playerId == null || !playersList.containsKey(playerId)) return false;
+        playersList.remove(playerId);
+        return true;
+    }
 
 
 }
--- src/test/java/GamePlayerManagerTest.java
+++ src/test/java/GamePlayerManagerTest.java
@@ -1,15 +1,101 @@
+import org.example.GamePlayerManager;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GamePlayerManagerTest {
 
+    static GamePlayerManager manager;
+    @BeforeAll
+    public static void setUp(){
+        manager = new GamePlayerManager();
+    }
 
     @Test
-    void registerPlayer(){
+    void registerPlayerAndGetPlayerNameByID(){
+        //Setup
+        String playerId = "1";
+        String playerName = "Player1";
+        //Execute
+        manager.registerPlayer(playerId, playerName);
+        //Assert
+        assertEquals(playerName, manager.getPlayerDetails(playerId));
+    }
 
+    @Test
+    public void getPlayerDetailsGetNameByPlayerId(){
+        //Setup
+        String playerId = "1";
+        String playerName = "Player1";
+        String expName = "Player1";
+        //Execute
+        manager.registerPlayer(playerId, playerName);
+        String actName = manager.getPlayerDetails(playerId);
+        //Assert
+        assertEquals(expName, actName);
+    }
 
-        //getplayerDetails()
+    @Test
+    public void getPlayerDetailsGetNameByPlayerIdNotFoundReturnNull(){
+        //Setup
+        String playerId = "1";
+        String playerName = "Player1";
+        String searchID = "99";
+        String expName = null;
+        //Execute
+        manager.registerPlayer(playerId, playerName);
+        String actName = manager.getPlayerDetails(searchID);
+        //Assert
+        assertEquals(expName, actName);
+    }
+
+    //void updatePlayerScore(String playerId, int scoreToAdd);
+    @Test
+    public void updatePlayerScore(){
+        //Setup
+        String playerId = "1";
+        //Execute
+        //Assert
+
 
     }
 
+    //boolean checkLevelUp(String playerId);
+    @Test
+    public void checkLevelUp(){
+        //Setup
+        String playerId = "1";
+        //Execute
+        //Assert
+
+
+    }
+
+    @Test
+    public void deletePlayer_DeleteExistingPlayerAndGetTrue(){
+        //Setup
+        String playerId = "1";
+        String playerName = "Player1";
+        boolean expBool = true;
+        //Execute
+        manager.registerPlayer(playerId, playerName);
+        boolean actBool = manager.deletePlayer(playerId);
+        //Assert
+        assertEquals(expBool, actBool);
+    }
+
+    @Test
+    public void deletePlayer_DeleteNonExistingPlayerAndGetFalse(){
+        //Setup
+        String playerId = "1";
+        String playerName = "Player1";
+        String searchID = "99";
+        boolean expBool = false;
+        //Execute
+        manager.registerPlayer(playerId, playerName);
+        boolean actBool = manager.deletePlayer(searchID);
+        //Assert
+        assertEquals(expBool, actBool);
+    }
 
 }
